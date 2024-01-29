066ebeae46a7d1701efcda3755c0efa748016bd3
Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>
2024-01-29 17:18:55 +0100

    Add overloads for RelaticsWebservices.get_result()
    Signed-off-by: Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>

--- pyrelatics2/client.py
+++ pyrelatics2/client.py
@@ -10,6 +10,7 @@ import sys
 import tempfile
 import uuid
 import zipfile
+from typing import overload
 
 from suds.client import Client
 from suds.plugin import MessagePlugin
@@ -227,6 +228,15 @@ class RelaticsWebservices:
             auth = {"Authentication": {}}  # Default value, because Relatics doesn't like this to be empty
 
         return auth
+
+    @overload
+    def get_result(self, operation_name: str, parameters: ParametersOrNone = None, authentication: None | str | ClientCredential = None, auto_parse_response: bool = True) -> ExportResult: ...
+    @overload
+    def get_result(self, operation_name: str, parameters: ParametersOrNone = None, authentication: None | str | ClientCredential = None, auto_parse_response: bool) -> ExportResult: ...
+    @overload
+    def get_result(self, operation_name: str, parameters: ParametersOrNone = None, authentication: None | str | ClientCredential = None) -> ExportResult: ...
+    @overload
+    def get_result(self, operation_name: str, parameters: ParametersOrNone = None, authentication: None | str | ClientCredential = None, auto_parse_response: bool = False) -> SudsObject: ...
 
     def get_result(
         self,
