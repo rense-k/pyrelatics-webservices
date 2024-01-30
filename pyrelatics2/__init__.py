aef571c39691b3b0cafc82cb62174fa696b67222
Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>
2024-01-30 15:12:50 +0100

    Apply isort

--- pyrelatics2/__init__.py
+++ pyrelatics2/__init__.py
@@ -1,5 +1,9 @@
-from .client import ClientCredential, RelaticsWebservices
+from .client import ClientCredential
+from .client import RelaticsWebservices
 from .exceptions import TokenRequestError
-from .result_classes import ExportResult, ImportResult
-from .utils import suds_get, suds_get_as_list, suds_get_as_str
+from .result_classes import ExportResult
+from .result_classes import ImportResult
+from .utils import suds_get
+from .utils import suds_get_as_list
+from .utils import suds_get_as_str
 from .version import __version__
