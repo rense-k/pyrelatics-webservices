be2a05a85ab542c2fb84911ea4d9aa7a894fee00
Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>
2024-01-30 22:26:46 +0100

    Revert "Fix crash when Import return a single element"
    This reverts commit 8a5d588ff08bc4a7c3a938498d0b2cfd19273f9c.

--- pyrelatics2/result_classes.py
+++ pyrelatics2/result_classes.py
@@ -227,11 +227,7 @@ class ImportResult(BaseResult):
 
             # Add all the elements when available
             if hasattr(suds_response.Import, "Elements") and len(suds_response.Import.Elements) > 0:
-                # Force a single element into a list and None into an empty list
-                _elements = suds_response.Import.Elements[0]
-                elements = _elements if isinstance(_elements, list) else [] if _elements is None else [_elements]
-
-                for elem in elements:
+                for elem in suds_response.Import.Elements[0]:
                     result.elements.append(
                         ImportElement(action=elem._Action, id=elem._ID, foreign_key=elem._ForeignKey)
                     )
