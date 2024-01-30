70330397cde7f4877277035063cdc6e33909047c
Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>
2024-01-30 22:26:46 +0100

    Revert "Add utility functions"
    This reverts commit 59bc6268cb5576746f2f95790a23d8500e15f5d0.

--- pyrelatics2/__init__.py
+++ pyrelatics2/__init__.py
@@ -1,5 +1,4 @@
 from .client import ClientCredential, RelaticsWebservices
 from .exceptions import TokenRequestError
 from .result_classes import ExportResult, ImportResult
-from .utils import suds_get, suds_get_as_list, suds_get_as_str
 from .version import __version__
