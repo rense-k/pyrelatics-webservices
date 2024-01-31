01d36552f8679b611cdc61d2b37f7b76d3222aeb
Rense Klinkenberg <4151359+rense-k@users.noreply.github.com>
2024-01-31 02:00:13 +0100

    Fix coverage script

--- run_coverage.bat
+++ run_coverage.bat
@@ -1,3 +1,4 @@
-coverage run --branch -m unittest discover
+coverage run --branch -m unittest discover tests
+@REM coverage run -m unittest discover tests
 coverage html --show-context
 coverage report -m
